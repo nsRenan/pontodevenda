86244702d7a9cdb9a6231d701bcbfe4b93eed203
Renan Nobre <renannobresan@gmail.com>
2023-11-20 11:46:03 -0300

    Instalação do Swagger

--- src/servidor.js
+++ src/servidor.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const rotas = require("./rotas/rotas");
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json')
+
 const app = express();
 
 app.use(express.json());
 
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 app.use(rotas);
+
 
 module.exports = app;
